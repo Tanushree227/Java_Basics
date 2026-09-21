fa8e6d99b630e2003034c0fa5ea9c73f2551949a
Tanushree227 <tanushreedas813@gmail.com>
2026-09-21 12:24:22 +0530

    if condition

---
+++ prg_conditional.java
@@ -0,0 +1,11 @@
+public class prg_conditional {
+    public static void main(String[] args)
+    {
+        int age = 18;
+        if(age >= 28)
+        {
+            System.out.println("Eligible for marriage");
+        }
+        System.out.println("Program executed successfully");
+    }
+}
