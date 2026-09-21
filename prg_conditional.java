efec4dcd141bc226819f796c3755cc3098ff5e14
Tanushree227 <tanushreedas813@gmail.com>
2026-09-21 12:33:44 +0530

    if else conditions

--- prg_conditional.java
+++ prg_conditional.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class prg_conditional {
     public static void main(String[] args)
     {
-        int age = 18;
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
         if(age >= 28)
         {
             System.out.println("Eligible for marriage");
+        }
+
+        int i = sc.nextInt();
+        if(i % 2 == 0)
+        {
+            System.out.println("Even number");
+        }
+        else
+        {
+            System.out.println("Odd number");
         }
         System.out.println("Program executed successfully");
     }
